e279264e305ebef0e37d461f91fe3d94cf39a312
Marcin Zapala <marcin.zapala.programista@gmail.com>
2019-03-10 19:42:39 +0100

    logowanie i rejestracja obiektowo

--- uzytkownik.h
+++ uzytkownik.h
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void podzielString(string linia, char znak, vector<string> &wyrazy);
 
 class Uzytkownik{
 
@@ -17,4 +20,22 @@ private:
 
     int id;
     string login, haslo;
+};
+
+class Uzytkownicy :public Uzytkownik{
+
+public:
+
+    Uzytkownicy(string nazwaPliku = "Uzytkownicy.txt");
+    int zwrocIdUzytkownika() const;
+    int zwrocLiczbeUzytkownikow() const;
+    void zaloguj();
+    void wyloguj();
+    void zarejestrujUzytkownika(string nazwaPliku);
+    void zmienHaslo(string nazwaPliku);
+
+private:
+
+    int idUzytkownika, liczbaUzytkownikow;
+    vector <Uzytkownik> uzytkownicy;
 };
